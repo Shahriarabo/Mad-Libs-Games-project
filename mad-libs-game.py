613cb889cf61874f3163bbd05b43fd1635dd68f7
Abdullah al shahriar <103172072+Shahriarabo@users.noreply.github.com>
2023-08-16 23:20:21 +0600

    Add files via upload

---
+++ mad-libs-game.py
@@ -0,0 +1,12 @@
+name = input("What is Your name : ")
+fathersname = input("What is Your fathers name : ")
+mothersname = input("What is Your mothers name : ")
+study = input("Which class do you read in : ")
+relationship = input("Tell ma about your relationship status : ")
+
+
+print(f"My cat Friend name is {name}, cat  Fathers name is {fathersname},cat Mothers name is {mothersname},"
+      f" Cat read in class {study}, cat Relationship status is {relationship}"
+      )
+
+
